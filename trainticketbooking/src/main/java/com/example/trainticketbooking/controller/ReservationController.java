112aa56d9bee09a3b16197da0a306c83ec9f2e15
lunerave <lunerave07@naver.com>
2024-08-30 14:35:49 +0900

    Front and Code Implementation: implement ticket booking page

---
+++ trainticketbooking/src/main/java/com/example/trainticketbooking/controller/ReservationController.java
@@ -0,0 +1,14 @@
+package com.example.trainticketbooking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ReservationController {
+
+    @GetMapping("/booking")
+    public String index() {
+        return "reservation";
+    }
+
+}
