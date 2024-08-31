3584513cd792038b61e214ee8750daef8e227428
lunerave <lunerave07@naver.com>
2024-08-31 16:42:50 +0900

    Code Implementation: implement reserve API while booking

--- trainticketbooking/src/main/java/com/example/trainticketbooking/controller/ReservationController.java
+++ trainticketbooking/src/main/java/com/example/trainticketbooking/controller/ReservationController.java
@@ -1,14 +1,35 @@
 package com.example.trainticketbooking.controller;
 
+import com.example.trainticketbooking.service.ReservationService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
 
     @GetMapping("/booking")
     public String index() {
         return "reservation";
     }
+
+    @PostMapping("/reserve")
+    public String reserve(@RequestParam String userId,
+                          @RequestParam Long trainId,
+                          @RequestParam Long startStationId,
+                          @RequestParam Long endStationId, Model model) {
+        boolean success = reservationService.reserve(userId, trainId, startStationId, endStationId);
+        model.addAttribute("success", success);
+        return "reservation";
+    }
+
 
 }
